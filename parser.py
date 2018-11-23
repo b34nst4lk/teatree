75c0834e10126c8f2ba3bb4a075a7b0e3bf873bd
Jack <sim.jack@accenture.com>
2018-11-23 17:10:04 +0800

    added parsing logic for activities, tasks and rules

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 from pprint import pprint as prettyprint
-from config import ns0, file_path
+from config import ns0, file_path_nip, file_path_hsbb
 
 def pprint(string):
     # prints out the node in an indented fashion
@@ -10,11 +10,36 @@ def gen_tag(string):
     # helper for generating strings for searching tags
     return '%s%s' % (ns0, string)
 
+nip = ET.parse(file_path_nip)
+hsbb = ET.parse(file_path_hsbb)
 
-new_installation = ET.parse(file_path)
+output = []
+activities = {}
+for activity in nip.findall(gen_tag('activity')):
+    activities[activity.get('name')] = activity
+    output.append(activity)
 
-root = new_installation.getroot()
-activities = root.findall(gen_tag('activity'))
-activity_names = [activity.attrib['name'] for activity in activities]
-print(len(activities))
-pprint(ET.tostring(activities[0]))
+cartridge = hsbb.find(gen_tag('cartridge'))
+tasks = {}
+for task in cartridge.iter(gen_tag('task')):
+    if task.get('name') is not None:
+        tasks[task.get('name')] = task
+
+rules = {}
+for rule in cartridge.iter(gen_tag('rule')):
+    if rule.get('name') is not None:
+        rules[rule.get('name')] = task
+
+for activity_name in activities:
+    if activity_name in tasks:
+        output.append(tasks[activity_name])
+    if activity_name in rules:
+        output.append(rules[activity_name])
+
+print(len(output))
+print(output[-1])
+pprint(ET.tostring(output[-1]))
+
+# for activity in activity_names:
+#     process = hsbb.find('.//%s//%s[@name="%s"]' % (gen_tag('cartridge'), gen_tag('process'), activity))
+#     print(process)
