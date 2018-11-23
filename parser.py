b8197ebcb8bb60ead820e02165b3a179a798270a
Jack <sim.jack@accenture.com>
2018-11-23 15:12:19 +0800

    added comments for helper functions

--- parser.py
+++ parser.py
@@ -3,10 +3,12 @@ from pprint import pprint as prettyprint
 from config import ns0
 
 def pprint(string):
+    # prints out the node in an indented fashion
     prettyprint(string.decode('utf-8'))
 
 def gen_tag(string):
-     return '%s%s' % (ns0, string)
+    # helper for generating strings for searching tags
+    return '%s%s' % (ns0, string)
 
 file_path = '../TM Uplift/process/A4.11_HOTES_New_Installation_Process.xml'
 
