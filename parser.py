816d6a6c595b82910949e89b6343b60caf28e2de
Jack <sim.jack@accenture.com>
2018-11-23 15:09:00 +0800

    setup: initial commit

---
+++ parser.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree as ET
+from pprint import pprint as prettyprint
+from config import ns0
+
+def pprint(string):
+    prettyprint(string.decode('utf-8'))
+
+def gen_tag(string):
+     return '%s%s' % (ns0, string)
+
+file_path = '../TM Uplift/process/A4.11_HOTES_New_Installation_Process.xml'
+
+new_installation = ET.parse(file_path)
+
+root = new_installation.getroot()
+activities = root.findall(gen_tag('activity'))
+activity_names = [activity.attrib['name'] for activity in activities]
+print(len(activities))
+pprint(ET.tostring(activities[0]))
